f82379edd539fe75b9e4e4707ed3fb450eb6ddbf
Gyuwon Seol <seolcu0112@proton.me>
2024-08-23 20:06:32 +0900

    switch to gnome

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -10,8 +10,8 @@
   imports = [
     # Desktop environment modules
     # ../../homeModules/hyprland.nix
-    ../../homeModules/plasma.nix
-    # ../../homeModules/gnome.nix
+    # ../../homeModules/plasma.nix
+    ../../homeModules/gnome.nix
 
     ../../homeModules/virt-manager.nix
   ];
