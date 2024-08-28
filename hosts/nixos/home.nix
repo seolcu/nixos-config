b00cc8842e9e31868ea282d4cee3ab6ef0946c94
Gyuwon Seol <seolcu0112@proton.me>
2024-08-28 18:14:16 +0900

    added UEFI support for QEMU/KVM

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -138,6 +138,12 @@
       #   org.gradle.console=verbose
       #   org.gradle.daemon.idletimeout=3600000
       # '';
+
+      ".config/libvirt/qemu.conf".text = ''
+        # Adapted from /var/lib/libvirt/qemu.conf
+        # Note that AAVMF and OVMF are for Aarch64 and x86 respectively
+        nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]
+      '';
     };
 
     # Home Manager can also manage your environment variables through
--- nixosModules/virt-manager.nix
+++ nixosModules/virt-manager.nix
@@ -1,5 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.virt-manager.enable = true;
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
+  };
+
+  # For nested virtualization
+  boot.extraModprobeConfig = "options kvm_intel nested=1";
 }
