5e844a27faf0f80577fc622fdd464bd9a7f5c9e3
Gyuwon Seol <seolcu0112@proton.me>
2024-09-03 22:37:51 +0900

    modularized locale

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -44,26 +44,14 @@
     ../../nixosModules/hostname.nix
     ../../nixosModules/networkmanager.nix
 
+    # i18n
+    ../../nixosModules/locale.nix
+
     # Nix config
     ../../nixosModules/nix-config.nix
   ];
 
   time.timeZone = "Asia/Seoul";
-
-  i18n = {
-    defaultLocale = "ko_KR.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "ko_KR.UTF-8";
-      LC_IDENTIFICATION = "ko_KR.UTF-8";
-      LC_MEASUREMENT = "ko_KR.UTF-8";
-      LC_MONETARY = "ko_KR.UTF-8";
-      LC_NAME = "ko_KR.UTF-8";
-      LC_NUMERIC = "ko_KR.UTF-8";
-      LC_PAPER = "ko_KR.UTF-8";
-      LC_TELEPHONE = "ko_KR.UTF-8";
-      LC_TIME = "ko_KR.UTF-8";
-    };
-  };
 
   programs = {
     firefox.enable = true;
