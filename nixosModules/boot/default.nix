f0866b2efc6e6dc668ccdbb5ec18913fe222d3fd
Gyuwon Seol <seolcu0112@proton.me>
2024-09-04 15:14:25 +0900

    added kernel config

--- nixosModules/boot/default.nix
+++ nixosModules/boot/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./cachyos-kernel.nix
+    ./kernel-config.nix
     ./plymouth.nix
     ./systemd-boot.nix
   ];
---
+++ nixosModules/boot/kernel-config.nix
@@ -0,0 +1,12 @@
+{ config, ... }:
+{
+  boot = {
+    # This is for OBS Virtual Cam Support
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    # Needed For Some Steam Games
+    kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+  };
+}
