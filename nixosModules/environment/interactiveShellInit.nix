0b7cb9392ff0e8e0db300502084a36853fdc6264
Gyuwon Seol <seolcu0112@proton.me>
2024-09-13 02:43:16 +0900

    added flatpak update command to up alias

--- nixosModules/environment/interactiveShellInit.nix
+++ nixosModules/environment/interactiveShellInit.nix
@@ -2,6 +2,6 @@
 {
   # Shell aliases
   environment.interactiveShellInit = ''
-    alias up='sudo nixos-rebuild switch --flake ~/nixos-config --upgrade'
+    alias up='sudo nixos-rebuild switch --flake ~/nixos-config --upgrade; flatpak update'
   '';
 }
