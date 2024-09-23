e96594cabbc243ecc27158654d34320a4fc968d3
Gyuwon Seol <seolcu0112@proton.me>
2024-09-23 14:59:07 +0900

    added sictools alias

--- nixosModules/environment/interactiveShellInit.nix
+++ nixosModules/environment/interactiveShellInit.nix
@@ -3,5 +3,6 @@
   # Shell aliases
   environment.interactiveShellInit = ''
     alias up='sudo nixos-rebuild switch --flake ~/nixos-config --upgrade; flatpak update'
+    alias sictools='java -jar ~/문서/코드/SicTools/out/make/sictools.jar'
   '';
 }
